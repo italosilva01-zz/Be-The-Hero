cbf73f264cdc5475a707b0f2c5df26fde28a5b42
italosilva01 <italo.silvan87@gmail.com>
2020-03-28 15:49:38 -0300

    I had problems with the master repository

---
+++ backend/src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
+
+const app = express();
+
+/*
+    Rota/Recurso
+
+    Métodos HTTP:
+
+    GET: Buscar uma informação do back-end
+    POST: Criar uma informação no back-end
+    PUT: Alterar uma informação do back-end
+    DELETE: Deletar uma informação no back-end
+
+
+    Tipos de parâmetros:
+
+    Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
+    Route Params: Parâmetros utilizados para identificar recursos
+    Request body: Corpo da requisição, utilizado para criar ou alterar
+
+*/
+//Informando para todas as rotas que irão receber um arquivo json
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+app.listen(3333);
